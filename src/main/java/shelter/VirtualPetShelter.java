b2385c581308bd147d523d076dca5e8f24c40650
JGibson37 <Jason.Gibson15@yahoo.com>
2020-05-22 14:27:43 -0400

    created map and return all values, XTDD

--- src/main/java/shelter/VirtualPetShelter.java
+++ src/main/java/shelter/VirtualPetShelter.java
@@ -1,6 +1,15 @@
 package shelter;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class VirtualPetShelter {
 
+    Map<String, VirtualPet> animals = new HashMap<>();
+
+    public Collection<VirtualPet> getAllAnimals(){
+        return animals.values();
+    }
 }
 
--- src/test/java/shelter/VirtualPetShelterTest.java
+++ src/test/java/shelter/VirtualPetShelterTest.java
@@ -1,6 +1,13 @@
 package shelter;
 
 
+import org.junit.jupiter.api.Test;
+
 public class VirtualPetShelterTest {
+    @Test
+    public void retrieveCollection(){
+        VirtualPetShelter underTest = new VirtualPetShelter();
+
+    }
 
 }
